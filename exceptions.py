1b8b5121d8cf0da2bda9bc177b21a601b41bd1f4
witold <witoldwrob@gmail.com>
2022-06-06 14:48:24 +0200

    fixes wrong return when 403

--- exceptions.py
+++ exceptions.py
@@ -1,4 +1,5 @@
-from fastapi import HTTPException, Request, status
+from fastapi import Request, status
+from fastapi.responses import JSONResponse
 
 
 class NoProjectMembershipException(Exception):
@@ -8,7 +9,7 @@ class NoProjectMembershipException(Exception):
 async def no_project_membership_exception_handler(
     request: Request, exc: NoProjectMembershipException
 ):
-    raise HTTPException(
+    return JSONResponse(
         status_code=status.HTTP_403_FORBIDDEN,
         detail="User does not have access to this project",
     )
