d34282f4d9ac4b6dd97406b96246866aecd534b9
witold <witoldwrob@gmail.com>
2022-06-20 18:46:46 +0200

    set logging level to INFO for scripts

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -6,7 +6,7 @@ import sys
 def get_logger(name: str):
     logger = logging.getLogger(name)
     logger.setLevel(
-        logging.getLevelName(os.getenv("EUPHROSYNE_TOOLS_LOGGING_LEVEL", "WARNING"))
+        logging.getLevelName(os.getenv("EUPHROSYNE_TOOLS_LOGGING_LEVEL", "INFO"))
     )
     logger.addHandler(logging.StreamHandler(sys.stdout))
     return logger
