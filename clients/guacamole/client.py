462d148fe245d033ad91001baa89022cbec0637b
Maxime Cattet <maximecattet@gmx.com>
2022-10-04 11:41:56 +0200

    feat: Fix method and add from_date to customize it

--- clients/guacamole/client.py
+++ clients/guacamole/client.py
@@ -178,13 +178,14 @@ class GuacamoleClient:
                 f"Error getting response ({resp.status_code}): {resp.json()['message']}"
             )
 
-        data = GuacamoleConnectionsAndGroupsResponse(**resp.json())
+        data = GuacamoleConnectionsAndGroupsResponse.parse_obj(resp.json())
         return data
 
     def get_vm_to_shutdown(
         self,
         time_unused: timedelta = timedelta(minutes=30),
         skip_groups: list[str] = None,
+        from_date: datetime = None,
     ) -> list[str]:
         """
         Check all existing VM connections to see
@@ -195,7 +196,9 @@ class GuacamoleClient:
         time_unused: timedelta
             timedelta representing the time a VM has been to be unused to be shutdown
         skip_groups: list[str]
-            Group name to ignore
+            Group name to ignore. Default to "imagery"
+        from_date: datetime
+            Datetime from when to compare if VM have been unused
 
         Returns
         --------
@@ -208,7 +211,7 @@ class GuacamoleClient:
 
         groups_and_connections = self.get_connections_and_groups()
 
-        if len(groups_and_connections) <= 0:
+        if len(groups_and_connections.child_connection_groups) <= 0:
             return []
 
         projects_to_shutdown: list[str] = []
@@ -231,10 +234,13 @@ class GuacamoleClient:
                     # Nobody has been connected to this VM
                     continue
 
-                now = datetime.now()
+                # Make sure the `now` and `connection.last_active` are on the same timezone
+                if from_date is None:
+                    from_date = datetime.now(connection.last_active.tzinfo)
+
                 last_active_delta = connection.last_active + time_unused
 
-                if now > last_active_delta:
+                if from_date > last_active_delta:
                     projects_to_shutdown.append(connection.name)
 
         return projects_to_shutdown
